08dfc8af1d9a1ab5ea405524894d032b91799f39
Damon Castillo <damono7.castillo@gmail.com>
2022-07-07 09:38:14 -0500

    3rd commit

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,3 +1,9 @@
 USE codeup_test_db;
 
+SELECT * FROM albums;
+SELECT * FROM albums WHERE release_date < 1980;
 SELECT * FROM albums WHERE artist = 'Micheal Jackson';
+
+UPDATE albums SET sales * 10;
+UPDATE albums SET release_date 1980 = 1800s;
+UPDATE albums SET artist 'Micheal Jackson' = 'Peter Jackson';
