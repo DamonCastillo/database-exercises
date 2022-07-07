54d761b422ba57913eeda5c5e8cf643f7b5d86a7
Damon Castillo <damono7.castillo@gmail.com>
2022-07-07 15:02:04 -0500

    completed where

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,7 @@
 USE employees;
 
 SELECT * FROM employees WHERE first_name IN ('Irena, Vidya, Maya');
-SELECT * FROM employees WHERE last_name LIKE '%E';
+SELECT * FROM employees WHERE last_name LIKE 'E%';
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 
 SELECT * FROM employees WHERE gender IN ('M');
@@ -12,7 +12,8 @@ SELECT * FROM employees WHERE LEFT(last_name,1) IN ('E')
 
 AND RIGHT(last_name,1) IN ('E');
 
-SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%'
+
+SELECT * FROM employees WHERE last_name LIKE '%q%'
 AND last_name NOT LIKE '%qu%'
 
 
